5878f1eb52bda6179629e24099db21c19fdbcbe9
Vito De Tullio <vito.detullio@gmail.com>
2025-01-03 10:31:00 +0100

    fillblanks

--- src/pqtdiff3/diff3.py
+++ src/pqtdiff3/diff3.py
@@ -1,6 +1,10 @@
 from difflib import SequenceMatcher
 from enum import Enum
 from enum import auto
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from collections.abc import Iterator
 
 
 class Common(Enum):
@@ -29,3 +33,52 @@ def get_commons(
                 else:  # old_acc
                     commons[j] = Common.all
     return commons
+
+
+def _fillblanks(
+    lines: list[str], commons: list[Common], others_commons: list[list[Common]]
+) -> 'Iterator[tuple[str, Common]]':
+    lines_it = iter(lines)
+    commons_it = iter(commons)
+    others_commons_its = [
+        iter(other_commons) for other_commons in others_commons
+    ]
+
+    while True:
+        line = next(lines_it, None)
+        if line is None:
+            break
+        common = next(commons_it)
+        if common is Common.all:
+            # search for holes
+            while True:
+                others = [
+                    next(other_commons_it, None)
+                    for other_commons_it in others_commons_its
+                ]
+                if all(other is Common.all for other in others):
+                    yield line, common
+                    break
+                # hole
+                yield '', Common.empty
+        else:
+            # consume others
+            for other_commons_it in others_commons_its:
+                next(other_commons_it, None)
+            yield line, common
+
+    # add an empty line at the end to match others lenghts
+    while True:
+        others_commons_dones = [
+            next(other_commons_it, None) is None
+            for other_commons_it in others_commons_its
+        ]
+        if all(others_commons_dones):
+            break
+        yield '', Common.empty
+
+
+def fillblanks(
+    lines: list[str], commons: list[Common], others_commons: list[list[Common]]
+) -> list[tuple[str, Common]]:
+    return list(_fillblanks(lines, commons, others_commons))
--- tests/test_diff3.py
+++ tests/test_diff3.py
@@ -1,17 +1,63 @@
 from unittest.case import TestCase
 
 from pqtdiff3.diff3 import Common
+from pqtdiff3.diff3 import fillblanks
 from pqtdiff3.diff3 import get_commons
 
 
 class TestGetCommons(TestCase):
     maxDiff = None
 
-    def test_updaterow(self) -> None:
+    def test_get_commons(self) -> None:
         a = ['common1', 'common2', 'changed', 'common3']
         b1 = ['common1', 'common2', 'onlybs', 'changedbs', 'common3']
         b2 = ['common1', 'common2', 'onlybs', 'changedbs', 'common3']
 
         expected = [Common.all, Common.all, Common.none, Common.all]
         actual = get_commons(a, [b1, b2], [Common.old_add, Common.old_acc])
+        self.assertListEqual(expected, actual)
+
+    def test_fillblanks(self) -> None:
+        lines = ['1com', '2cha', '3com', '8com']
+        commons = [Common.all, Common.none, Common.all, Common.all]
+        others_commons = [
+            [
+                Common.all,
+                Common.old_acc,
+                Common.all,
+                Common.old_acc,
+                Common.old_acc,
+                Common.old_acc,
+                Common.old_acc,
+                Common.all,
+                Common.old_acc,
+                Common.old_acc,
+            ],
+            [
+                Common.all,
+                Common.old_acc,
+                Common.all,
+                Common.old_acc,
+                Common.old_acc,
+                Common.old_acc,
+                Common.old_acc,
+                Common.all,
+                Common.old_acc,
+                Common.old_acc,
+            ],
+        ]
+
+        expected = [
+            ('1com', Common.all),
+            ('2cha', Common.none),
+            ('3com', Common.all),
+            ('', Common.empty),
+            ('', Common.empty),
+            ('', Common.empty),
+            ('', Common.empty),
+            ('8com', Common.all),
+            ('', Common.empty),
+            ('', Common.empty),
+        ]
+        actual = fillblanks(lines, commons, others_commons)
         self.assertListEqual(expected, actual)
